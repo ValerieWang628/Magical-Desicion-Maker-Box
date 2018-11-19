a6b92ea3b108de306c007fc5b327b90bda047604
Valerie Wang <34406500+ValerieWang628@users.noreply.github.com>
2018-11-19 01:40:35 -0500

    Update TP_RankBased_TestBoard.py

--- TP_RankBased_TestBoard.py
+++ TP_RankBased_TestBoard.py
@@ -37,4 +37,49 @@ rankMatrix3 = [ [ 4,  1,  2,  3 ],
 # the path is:
 # A-B: 8; A-C: 2; A-D:8
 # B-C: 2; B-D: 6
-# C-D: 6                                    
+# C-D: 6    
+
+##------------- below are for instant run-off elimination------------##
+## when there is a tie, all minimum vanish together
+
+rankMatrix4 = [ [ 2,  3,  4,  1 ],
+                ['A','B','D','C'],
+                ['B','C','A','A'],
+                ['C','D','B','B'],
+                ['D','A','C','D']
+                                    ]
+
+# the instant run-off elimination: eliminate C first, then A and B
+# winner is D!
+
+
+# this is a matrix w/ only one round of elimination
+rankMatrix5 = [ [ 2,  4,  1,  3 ],
+                ['A','B','D','D'],
+                ['B','C','A','A'],
+                ['C','D','B','B'],
+                ['D','A','C','C']
+                                    ]
+
+# the instant run-off elimination: 
+# first round: A:2, B:4, D:4;
+# second round: B:6, D:4
+# B wins!
+
+
+# this is a template board that player sequence in each attribute 
+# is organized as a bitig snake
+# weights can be assigned arbitrarily to test the algorithms
+
+rankMatrix6 = [ [ 3,  3,  2,  2 ],
+                ['A','B','C','D'],
+                ['B','C','D','A'],
+                ['C','D','A','B'],
+                ['D','A','B','C']
+                                    ]
+
+# the IRV:
+# first round: C,D out
+# Second round:C,D's vote given A
+# A:7 B:3
+# A wins
