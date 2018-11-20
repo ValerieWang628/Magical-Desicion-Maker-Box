c4b12c9fc054c157f2a44c75106975276e178fa6
Valerie Wang <34406500+ValerieWang628@users.noreply.github.com>
2018-11-20 15:42:17 -0500

    Update TP_RankBased_SchulzeBeatPath.py

--- TP_RankBased_SchulzeBeatPath.py
+++ TP_RankBased_SchulzeBeatPath.py
@@ -103,149 +103,25 @@ def smithSetFinder(matrix, playerList):
         to get the winner set -- the smith set '''
     playerSet = set(playerList)
     underdogSet, overriderSet, scoreBeatList = underdogOverriderFinder(matrix,playerList)
-    # if overriderSet != set(): return overriderSet
-    playerSet -= underdogSet
-    dominantContainer = []
-    for _ in playerSet:
-        tmpSmith = set()
-        for player in playerSet:
-            tmpSmith.add(player)
-            if isDominantSet(tmpSmith,playerSet,scoreBeatList):
-                dominantContainer.append(tmpSmith)
-    return min(dominantContainer, key = len)
+    if overriderSet != set(): return overriderSet
+    else:
+        playerSet -= underdogSet
+        dominantContainer = []
+        playerList = list(playerSet)
+        for i in range(len(playerList)):
+            tmpSmith = set()
+            for player in playerList[i:]:
+                tmpSmith.add(player)
+                print("tmp", tmpSmith)
+                if isDominantSet(tmpSmith,playerSet,scoreBeatList):
+                    dominantContainer.append(tmpSmith.copy())
+                    print("domin", dominantContainer)
+        return min(dominantContainer, key = len)
 
 print(smithSetFinder(matrix,playerList))
 
 
-'''
-ignore_rest: this is the tmp code.
 
-
-import copy
-
-playerList = ['A','B','C','D']
-
-matrix = [  [ 4,  1,  2,  3 ],
-            ['A','C','A','C'],
-            ['B','B','D','A'],
-            ['C','D','B','B'],
-            ['D','A','C','D']
-                                    ]
-def pathIdentifier(matrix, playerList):
-    combSet = set() # initializing an empty set for all combinations
-    for i in range(len(playerList)):
-        for j in range(i+1,len(playerList)):
-            combSet.add((playerList[i], playerList[j]))
-    rows, cols = len(matrix), len(matrix[0])
-    scoreList = []
-    for pair in combSet:
-        score = dict.fromkeys(pair, 0)
-        for player in pair:
-            for c in range(cols):
-                for r in range(1,rows):
-                    if matrix[r][c] == player:
-                        score[player] += matrix[0][c]
-                        break
-                    if matrix[r][c] in pair:
-                        score[matrix[r][c]] += matrix[0][c]
-                        break
-            break
-        scoreList.append(score)
-    return scoreList, combSet
-
-def positiveBeatFinder(matrix, playerList):
-    scoreList, combSet = pathIdentifier(matrix, playerList)
-    scoreBeatList = []
-    for score in scoreList:
-        scoreBeat = score.copy()
-        # this is to prevent the original score from being modified
-        # scoreBeat will only record positive path
-        # and set loser beat to 0
-        for player in scoreBeat:
-            # ties get 0 for both direction
-            if score[player] == max(score.values()):
-                scoreBeat[player] -= sorted(score.values())[0]
-            else: scoreBeat[player] = 0
-        scoreBeatList.append(scoreBeat)
-    return scoreBeatList
-
-
-def underdogOverriderFinder(matrix,playerList):
-    playerSet = set(playerList)
-    scoreBeatList = positiveBeatFinder(matrix,playerList)
-    atLeastOneWin, atLeastOneLose = set(),set()
-    for scoreBeat in scoreBeatList:
-        for player in scoreBeat:
-            if scoreBeat[player] != 0:
-                atLeastOneWin.add(player)
-            else: atLeastOneLose.add(player)
-    underdogSet = playerSet - atLeastOneWin
-    overriderSet = playerSet - atLeastOneLose
-    return underdogSet, overriderSet, scoreBeatList
-
-def isDominantSet(tmpSmith, playerSet, scoreBeatList):
-    overflowSet = playerSet - tmpSmith
-    for scoreBeat in scoreBeatList:
-        for player in scoreBeat:
-            if (player in tmpSmith
-                and scoreBeat[player] == 0
-                and max(scoreBeat, key = scoreBeat.get) in overflowSet):
-                return False
-    return True
-
-def smithSetFinder(matrix, playerList):
-    '''this func uses the positive defeat score to eliminate losers
-        to get the winner set -- the smith set '''
-    playerSet = set(playerList)
-    underdogSet, overriderSet, scoreBeatList = underdogOverriderFinder(matrix,playerList)
-    # if overriderSet != set(): return overriderSet
-    playerSet -= underdogSet
-    dominantContainer = []
-    dominantSet = set()
-    for _ in playerSet:
-        tmpSmith = set()
-        for player in playerSet:
-            tmpSmith.add(player)
-            print("tmp", tmpSmith)
-            if isDominantSet(tmpSmith,playerSet,scoreBeatList):
-                dominantSet = tmpSmith.copy()
-                dominantContainer.append(dominantSet)
-                print("domin", dominantContainer)
-    return min(dominantContainer, key = len)
-
-print(smithSetFinder(matrix,playerList))
-
-
-'''
-
-'''
-new viable algorithm:
-
-def smithSetFinder(matrix, playerList):
-    '''this func uses the positive defeat score to eliminate losers
-        to get the winner set -- the smith set '''
-    playerSet = set(playerList)
-    underdogSet, overriderSet, scoreBeatList = underdogOverriderFinder(matrix,playerList)
-    # if overriderSet != set(): return overriderSet
-    playerSet -= underdogSet
-    dominantContainer = []
-    dominantSet = set()
-    tmpSmith = set()
-    playerList = list(playerSet)
-    for i in range(len(playerList)):
-        tmpSmith = set()
-        for player in playerList[i:]:
-            tmpSmith.add(player)
-            print("tmp", tmpSmith)
-            if isDominantSet(tmpSmith,playerSet,scoreBeatList):
-                dominantSet = tmpSmith.copy()
-                dominantContainer.append(dominantSet)
-                print("domin", dominantContainer)
-    return min(dominantContainer, key = len)
-
-
-
-'''
 
    
 
