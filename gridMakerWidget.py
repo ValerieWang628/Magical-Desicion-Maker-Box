dfe685882f8863e36cb3d457a10cf8dbd5a1edf9
Valerie Wang <34406500+ValerieWang628@users.noreply.github.com>
2018-11-26 01:57:31 -0500

    Update gridMakerWidget.py

--- gridMakerWidget.py
+++ gridMakerWidget.py
@@ -1,20 +1,38 @@
 #cited from https://stackoverflow.com/questions/44996633/accessing-entry-widget-created-using-for-loop
 from tkinter import *
-
+import topsis
 
 class GridLayer():
 
     @staticmethod
     def run(data):
+
+        def keyPressed(event, data, rows, cols, top):
+            if len(data.entryStorage) == rows * cols:
+                top.destroy()
+                topsis.Topsis().run(data)
+
+        def store(e, storageList):
+            storageList.append(e.widget.get())
+
         top = Toplevel()
         top.title("GridMaker")
-        col = int(data.stringInput2)
-        row = int(data.stringInput3)
+        top.bind("<Key>", lambda event:
+                    keyPressed(event, data, rows, cols, top))
+        cols = int(data.stringInput2)
+        rows = int(data.stringInput3)
         entries = []
-        for i in range(row): 
+        for i in range(1, rows+1):
+            Label(top, text = str(i)).grid(row = i)
+        for j in range(1, cols+1):
+            Label(top, text = str(j)).grid(row = 0, column = j)
+        for i in range(rows): 
             newrow = []
-            for j in range(col): 
-                b = Entry(top, text="",width=8)
-                b.grid(row=i, column=j)
+            for j in range(cols): 
+                b = Entry(top, text="",width=15)
+                b.grid(row=i+1, column=j+1)
+                b.bind("<Return>", lambda e: store(e, data.entryStorage))
                 newrow.append(b)
             entries.append(newrow)
+
+
