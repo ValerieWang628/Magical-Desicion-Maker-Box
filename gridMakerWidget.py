65b48beb8db2de5f0a5846e419ba7b88520a4e9e
Valerie Wang <34406500+ValerieWang628@users.noreply.github.com>
2018-11-27 14:15:14 -0500

    Update gridMakerWidget.py

--- gridMakerWidget.py
+++ gridMakerWidget.py
@@ -24,10 +24,12 @@ class GridLayer():
         top.bind("<Key>", lambda event:
                     keyPressed(event, data, top))
         entries = []
-        for i in range(1, data.cols+1):
+        for i in range(1, data.rows+1):
+            print(data.alternative[i-1])
             Label(top, text = data.alternative[i-1]).grid(row = i)
-        for j in range(1, data.rows+1):
-            Label(top, text = data.attribute[i-1]).grid(row = 0, column = j)
+        for j in range(1, data.cols+1):
+            print(data.attribute[j-1])
+            Label(top, text = data.attribute[j-1]).grid(row = 0, column = j)
         for i in range(data.rows): 
             newrow = []
             for j in range(data.cols): 
