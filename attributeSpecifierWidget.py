b70aae0259774ef3715163c47cef7d7595b10d42
Valerie Wang <34406500+ValerieWang628@users.noreply.github.com>
2018-11-28 02:17:58 -0500

    Update attributeSpecifierWidget.py

--- attributeSpecifierWidget.py
+++ attributeSpecifierWidget.py
@@ -1,7 +1,5 @@
 #cited from https://stackoverflow.com/questions/44996633/accessing-entry-widget-created-using-for-loop
 from tkinter import *
-import topsis
-import weightSumWidget
 import gridMakerWidget
 
 class AttributeSpecifier():
@@ -13,22 +11,23 @@ class AttributeSpecifier():
             if len(data.attribute) == data.cols:
                 top.destroy()
                 gridMakerWidget.GridLayer().run(data)
-                # weightSumWidget.WeightSumMaker().run(data)
-
 
         def store(e, data):
             data.attribute.append(e.widget.get())
 
         top = Toplevel()
-        top.title("Weighted Sum")
+        top.title("Please Enter Attributes.")
+        top.geometry("500x500+300+0")
         top.bind("<Key>", lambda event:
             keyPressed(event, data, top))
+        Label(top, text = "\n",font = "Helvetica 15").grid(row = 0, columnspan = 2,column = 1, sticky = W)
+        Label(top, text = " We Would Love to Know What Attributes You Have in Mind.").grid(row = 1, columnspan = 2, sticky = W)
+        Label(top, text = " Please Enter Consider Quantitative Attributes Only.").grid(row = 2, columnspan = 2, sticky = W)
+        Label(top, text = "\n",font = "Helvetica 15").grid(row = 3, columnspan = 2,column = 1, sticky = W)
 
         for i in range(data.cols):
-            Label(top, text = "Attribute %s" % str(i+1) ).grid(row = i, column = 0)
-            b = Entry(top, text = "", width = 10)
-            b.grid(row = i, column = 1)
+            Label(top, text = "Attribute %s" % str(i+1) ).grid(row = i+5, column = 0)
+            b = Entry(top, text = "", width = 20)
+            b.grid(row = i+5, column = 1)
             b.bind("<Return>", lambda e: store(e, data))
 
-
-            
