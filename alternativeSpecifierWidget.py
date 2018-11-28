8ddb324f7912797e647d3ef36783f26610cd1a2a
Valerie Wang <34406500+ValerieWang628@users.noreply.github.com>
2018-11-27 22:04:50 -0500

    Update alternativeSpecifierWidget.py

--- alternativeSpecifierWidget.py
+++ alternativeSpecifierWidget.py
@@ -22,11 +22,12 @@ class AlternativeSpecifier():
         top.title("Alternative Specifier")
         top.bind("<Key>", lambda event:
             keyPressed(event, data, top))
-
+        Label(top, text = "We Would Love to Know What Alternatives You Have in Mind.").grid(row = 0, columnspan = 2, sticky = W)
+        Label(top, text = "Please Use A Capitalized Letter to Represent The Options. ").grid(row = 1, columnspan = 2, sticky = W)
+        
         for i in range(data.rows):
-            Label(top, text = "Alternative %s" % str(i+1)).grid(row = i, column = 0)
-            b = Entry(top, text = "", width = 10)
-            b.grid(row = i, column = 1)
+            Label(top, text = "Alternative %s" % str(i+1)).grid(row = i+2, column = 0)
+            b = Entry(top, text = "", width = 20)
+            b.grid(row = i+2, column = 1)
             b.bind("<Return>", lambda e: store(e, data))
-        print(data.alternative)
 
